b3700d31bef2c68b4526e8bf2832610d88b7ce46
Bruno <62807696+Wick096@users.noreply.github.com>
2022-05-05 15:24:10 -0300

    Add files via upload

---
+++ Bootcamp Fullstack Impulso/Busca_e_Lacos/comunicacao_Parelas.js
@@ -0,0 +1,19 @@
+/**
+ * Comunicação em Piralândia
+ *
+ * Desafio:
+ * - Os cientistas brasileiros descobriram um exoplaneta 
+ * a 1 bilhão de anos luz da terra. Ele foi carinhosamente 
+ * batizado de Piralândia, uma homenagem a cidade de Piracicaba, 
+ * onde moram os cientistas. Após algum tempo observando esse 
+ * planeta, os cientistas puderam ver que nele haviam habitantes 
+ * e, além disso, esses habitantes estavam tentando fazer contato 
+ * com os cientistas através de uma comunicação numérica.
+ *
+ */
+
+var numeros = gets().split("");
+
+numeros.reverse();
+
+console.log(numeros.join(""));

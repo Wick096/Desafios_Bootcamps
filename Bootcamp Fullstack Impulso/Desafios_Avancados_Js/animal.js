2534a13ad8791db6037fd080c285617d230bba52
Bruno <62807696+Wick096@users.noreply.github.com>
2022-05-10 16:40:07 -0300

    Add files via upload

---
+++ Bootcamp Fullstack Impulso/Desafios_Avancados_Js/animal.js
@@ -0,0 +1,35 @@
+let x = gets(); 
+let y = gets(); 
+let z = gets(); 
+
+if ((x == "vertebrado") && (y == "ave")  && (z == "carnivoro")) {
+  console.log("aguia\n");
+}
+
+if ((x == "vertebrado") && (y == "ave")  && (z == "onivoro")) {
+  console.log("pomba\n");
+}
+
+if ((x == "vertebrado") && (y == "mamifero")  && (z == "onivoro")) {
+  console.log("homem\n");
+}
+
+if ((x == "vertebrado") && (y == "mamifero")  && (z == "herbivoro")) {
+  console.log("vaca\n");
+}
+
+if ((x == "invertebrado") && (y == "inseto")  && (z == "hematofago")) {
+  console.log("pulga\n");
+}
+
+if ((x ==  "invertebrado") && (y == "inseto")  && (z == "herbivoro")) {
+  console.log("lagarta\n");
+}
+
+if ((x  == "invertebrado") && (y == "anelideo") && (z == "hematofago")) {
+  console.log("sanguessuga\n");
+}
+
+if ((x  == "invertebrado") && (y == "anelideo") && (z == "onivoro")) {
+  console.log("minhoca\n");
+}
